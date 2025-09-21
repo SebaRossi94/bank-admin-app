25f7915678da3f8c4a5c6090223c843fb01facd1
SebaRossi94 <seba.rossi.94@gmail.com>
2025-09-21 01:44:14 -0300

    update account endpoints

--- backend/app/api/v1/accounts.py
+++ backend/app/api/v1/accounts.py
@@ -1,13 +1,16 @@
 from collections.abc import Sequence
 import logging
+import uuid
 
 from fastapi import status
 from fastapi.exceptions import HTTPException
 from fastapi.routing import APIRouter
-from sqlmodel import select
+from sqlmodel import select, or_
 
 from app.db import session_dependency
 from app.models.account import Account, AccountCreate, AccountRead
+from app.models.transference import Transference, TransferenceRead
+from app.models.customer import Customer
 
 router = APIRouter(prefix="/accounts", tags=["accounts"])
 
@@ -24,12 +27,17 @@ def get_all_accounts(session: session_dependency) -> Sequence[Account]:
 
 
 @router.post("/")
-def create_account(
-    account_data: AccountCreate, session: session_dependency
-) -> Account:
+def create_account(account_data: AccountCreate, session: session_dependency) -> Account:
     """
     Create Account
     """
+    customer = session.exec(
+        select(Customer).where(Customer.id == account_data.customer_id)
+    ).first()
+    if not customer:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Customer not found"
+        )
     created_account = Account(**account_data.model_dump())
     session.add(created_account)
     session.commit()
@@ -39,10 +47,30 @@ def create_account(
 
 @router.get("/{account_id}", response_model=AccountRead)
 def get_account_by_id(account_id: int, session: session_dependency) -> Account:
-    print(id)
+    """
+    Get account by id
+    """
     account = session.exec(select(Account).where(Account.id == account_id)).first()
     if not account:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND, detail="Account not found"
         )
     return account
+
+
+@router.get("/{account_number}/transferences", response_model=Sequence[TransferenceRead])
+def get_account_transferences_by_id(
+    account_id: uuid.UUID, session: session_dependency
+) -> Sequence[Transference]:
+    """
+    Get account transferences by account number
+    """
+    outgoing_transferences = session.exec(
+        select(Transference).where(
+            or_(
+                Transference.from_account_number == account_id,
+                Transference.to_account_number == account_id,
+            )
+        )
+    ).all()
+    return list(outgoing_transferences)
