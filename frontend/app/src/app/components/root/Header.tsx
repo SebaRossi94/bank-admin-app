91c0745d0d7a31dc9336d6d1df859fb6d2e45f7a
SebaRossi94 <seba.rossi.94@gmail.com>
2025-09-21 11:16:24 -0300

    add link to app header

--- frontend/app/src/app/components/root/Header.tsx
+++ frontend/app/src/app/components/root/Header.tsx
@@ -1,14 +1,16 @@
+import React from "react";
+import Link from "next/link";
+
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
-import React from "react";
 
 function Header() {
   return (
     <AppBar position="static">
       <Toolbar>
         <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-          Bank Admin
+          <Link href="/">Bank Admin</Link>
         </Typography>
       </Toolbar>
     </AppBar>
