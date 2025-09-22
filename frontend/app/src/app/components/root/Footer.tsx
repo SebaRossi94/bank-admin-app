f2f5e85956725223dd48c088459341601939bba2
SebaRossi94 <seba.rossi.94@gmail.com>
2025-09-21 22:55:41 -0300

    update footer

--- frontend/app/src/app/components/root/Footer.tsx
+++ frontend/app/src/app/components/root/Footer.tsx
@@ -1,8 +1,31 @@
-import { Container, Typography } from "@mui/material";
-import { Box } from "@mui/material";
+"use client";
 import React from "react";
+import { Stack } from "@mui/material";
+import { Box } from "@mui/material";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import MailIcon from "@mui/icons-material/Mail";
 
 function Footer() {
+  const icons = [
+    {
+      icon: GitHubIcon,
+      onClick: () => window.open("https://github.com/SebaRossi94", "_blank"),
+    },
+    {
+      icon: LinkedInIcon,
+      onClick: () =>
+        window.open(
+          "https://www.linkedin.com/in/sebastian-rossi-94/",
+          "_blank"
+        ),
+    },
+    {
+      icon: MailIcon,
+      onClick: () => window.open("mailto:seba.rossi.94@gmail.com", "_blank"),
+    },
+  ];
+
   return (
     <Box
       component="footer"
@@ -11,11 +34,15 @@ function Footer() {
         mt: "auto",
       }}
     >
-      <Container maxWidth="lg">
-        <Typography variant="body2" color="text.secondary" align="center">
-          © {new Date().getFullYear()} Bank App
-        </Typography>
-      </Container>
+      <Stack direction="row" spacing={2} justifyContent="space-between">
+        {icons.map((icon) => (
+          <icon.icon
+            color="primary"
+            onClick={icon.onClick}
+            sx={{ cursor: "pointer", ":hover": { color: "secondary" } }}
+          />
+        ))}
+      </Stack>
     </Box>
   );
 }
